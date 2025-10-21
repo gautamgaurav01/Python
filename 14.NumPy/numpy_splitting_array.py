1ad5e205db5597fa798ee966a21d224158392eb3
Gaurav Gautam <gautamgaurav2063@gmail.com>
2025-10-21 07:10:18 +0545

    update work

--- 14.NumPy/numpy_splitting_array.py
+++ 14.NumPy/numpy_splitting_array.py
@@ -4,10 +4,12 @@ Splitting is reverse operation of Joining.
 
 Joining merges multiple arrays into one and Splitting breaks one array into multiple.
 
-We use array_split() for splitting arrays, we pass it the array we want to split and 
+We use array_split() for splitting arrays, we pass it the array we want to split and
 the number of splits.
 """
+
 import numpy as np
+
 arr = np.array([1, 2, 3, 4, 5, 6])
 newarr = np.array_split(arr, 3)
 print(newarr)
@@ -15,6 +17,7 @@ print(newarr)
 
 #
 import numpy as np
+
 arr = np.array([1, 2, 3, 4, 5, 6])
 newarr = np.array_split(arr, 4)
 print(newarr)
@@ -24,6 +27,7 @@ Split Into Arrays
 The return value of the array_split() method is a list containing each of the split as an array.
 """
 import numpy as np
+
 arr = np.array([1, 2, 3, 4, 5, 6])
 newarr = np.array_split(arr, 3)
 print(newarr[0])
@@ -37,6 +41,7 @@ Use the same syntax when splitting 2-D arrays.
 Use the array_split() method, pass in the array you want to split and the number of splits you want to do. 
 """
 import numpy as np
+
 arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
 newarr = np.array_split(arr, 3)
 print(newarr)
@@ -44,19 +49,28 @@ print(newarr)
 
 #
 import numpy as np
-arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+arr = np.array(
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]]
+)
 newarr = np.array_split(arr, 3)
 print(newarr)
 
 
 #
 import numpy as np
-arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+arr = np.array(
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]]
+)
 newarr = np.array_split(arr, 3, axis=1)
 print(newarr)
 
 #
 import numpy as np
-arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+arr = np.array(
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]]
+)
 newarr = np.hsplit(arr, 3)
 print(newarr)
