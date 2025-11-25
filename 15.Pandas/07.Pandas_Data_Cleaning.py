ae8c3560b9ac2d87f274b636f60b9439d809ccf1
Gaurav Gautam <gautamgaurav2063@gmail.com>
2025-11-25 13:24:59 +0545

    update work

--- 15.Pandas/07.Pandas_Data_Cleaning.py
+++ 15.Pandas/07.Pandas_Data_Cleaning.py
@@ -9,8 +9,28 @@ of work done with Pandas is data cleaning
 df = pd.read_csv("data.csv")
 
 # 1. Drop Irrelevant Columns
+
 # df= df.drop(columns=["Legendary" , "No"])
+
 
 # Handle Missing Data
 
-print(df)
+# df= df.dropna(sunset= ["Type2"])
+# df= df.fillna({"Type2" : "None"})
+
+
+# Fix inconsistent values
+# df["Type1"] = df["Type1"].replace({"Grass": "GRASS", "Fire": "FIRE"})
+
+
+# 4. Standardize text
+# df["Name"]= df["Name"].str.lower()
+
+
+# 5. Fix data types
+# df["Legendary"] = df["Legendary"].astype(bool)
+
+
+# 6. Remove duplicate values
+df = df.drop_duplicates()
+print(df.to_sting())
